6bcc0da536be6b56e5d54fc0ce7854f71247bce4
Ubuntu <ubuntu@ip-172-31-38-61.us-east-2.compute.internal>
2019-10-12 19:19:29 +0000

    service item test images and content loading correctly - service.html page styled and site responsive in all screen resolutions

--- services/models.py
+++ services/models.py
@@ -4,7 +4,7 @@ from django.db import models
 class Service (models.Model):
     
     title = models.CharField(max_length=255)
-    service_image = models.ImageField(upload_to='images')
+    image = models.ImageField(upload_to='images')
     description = models.TextField()
     price = models.DecimalField(max_digits=6, decimal_places=2)
     quantity = models.IntegerField()
